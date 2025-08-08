c7954b43cadc591e35e7169337e562882ae6b792
dollaransh17 <ab6113013@gmail.com>
2025-08-08 18:44:41 +0530

    Deploy Flutter web app to GitHub Pages

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/coder.op/Flutter_Learn/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/coder.op/my_color_which_color/Color_guessing"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
